717dc0b7119f5326a3983f55794c47c084de51a9
Fabian Thorand <f.thorand@gmail.com>
2020-12-01 20:28:59 +0100

    Solve day01-2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,9 +1,15 @@
-use std::{error::Error, fs::File, collections::HashSet, io::{BufRead, BufReader}, path::Path};
+use std::{
+    error::Error,
+    fs::File,
+    io::{BufRead, BufReader},
+    path::Path,
+    path::PathBuf,
+    str::FromStr,
+};
 
 fn main() {
-    for arg in std::env::args_os().skip(1) {
-        let path = Path::new(&arg);
-        match process(path) {
+    if let Some((path, count)) = parse_args() {
+        match solve(&path, count) {
             Ok(result) => {
                 println!("{}: {}", path.display(), result);
             }
@@ -11,24 +17,85 @@ fn main() {
                 println!("{}: {}", path.display(), err);
             }
         }
+    } else {
+        eprintln!("Usage: aoc2020-day01 path/to/input <SIZE-OF-SUM>");
+        std::process::exit(1);
     }
 }
 
-fn process(path: &Path) -> Result<i32, Box<dyn Error>> {
+fn parse_args() -> Option<(PathBuf, usize)> {
+    let mut args = std::env::args_os().skip(1);
+    let path = args.next()?.into();
+    let count = args.next()?.to_str()?.parse().ok()?;
+    if args.next().is_none() {
+        Some((path, count))
+    } else {
+        None
+    }
+}
+
+fn solve(path: &Path, count: usize) -> Result<u32, Box<dyn Error>> {
+    let mut numbers: Vec<u32> = read_lines(path)?;
+
+    numbers.sort();
+    find_product(count, 2020, &numbers).ok_or(
+        std::io::Error::new(std::io::ErrorKind::InvalidData, "No numbers add up to 2020").into(),
+    )
+}
+
+fn find_product(count: usize, sum: u32, sorted_nums: &[u32]) -> Option<u32> {
+    if count == 0 {
+        if sum == 0 {
+            // With zero terms we can only form the empty sum `0`, and the empty product `1`.
+            Some(1)
+        } else {
+            None
+        }
+    } else if count == 1 {
+        // This case is not really needed, but allows for a small optimization:
+        // We can directly check whether the `sum` is part of the set,
+        // instead of checking whether there's an `x` in the set such that `sum - x == 0`.
+        if sorted_nums.binary_search(&sum).is_ok() {
+            // product of just this one number is itself
+            Some(sum)
+        } else {
+            None
+        }
+    } else {
+        // Assumption: There are no negative numbers.
+        // This allows us to disregard any numbers greater than `sum`, as those
+        // can never be part of a sum that adds up to exactly `sum`.
+        let cutoff = match sorted_nums.binary_search(&sum) {
+            Ok(pos) => pos + 1,
+            Err(pos) => pos,
+        };
+
+        for i in 0..cutoff {
+            let this_num = sorted_nums[i];
+            // We know sum >= all numbers below the cutoff point.
+            let remaining_sum = sum - this_num;
+            if let Some(remaining_prod) = find_product(count - 1, remaining_sum, &sorted_nums[0..i])
+            {
+                return Some(this_num * remaining_prod);
+            }
+        }
+        None
+    }
+}
+
+fn read_lines<P: AsRef<Path>, T: FromStr>(path: P) -> Result<Vec<T>, Box<dyn Error>>
+where
+    T::Err: Error + 'static,
+{
     let file = File::open(path)?;
     let reader = BufReader::new(file);
 
-    let mut seen = HashSet::new();
+    let mut numbers: Vec<T> = Vec::new();
 
     for line in reader.lines() {
-        let num = line?.parse::<i32>()?;
-        let complement = 2020 - num;
-        if seen.contains(&complement) {
-            return Ok(complement * num)
-        } else {
-            seen.insert(num);
-        }
+        let num = line?.parse::<T>()?;
+        numbers.push(num);
     }
 
-    Err(std::io::Error::new(std::io::ErrorKind::InvalidData, "No two numbers add up to 2020").into())
+    Ok(numbers)
 }
