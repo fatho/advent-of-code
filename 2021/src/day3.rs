5ff224e36c74becd5844170196d276510aa1c806
Fabian Thorand <f.thorand@gmail.com>
2021-12-03 06:23:21 +0100

    Solve day3.1

--- 2021/src/day3.rs
+++ 2021/src/day3.rs
@@ -1,15 +1,22 @@
 use crate::FileParser;
+use std::cmp::Ordering;
 use std::io::Read;
 use std::str::FromStr;
 
 pub fn part1(input: &mut dyn Read) -> anyhow::Result<()> {
     let mut parser = FileParser::new(input);
+    let mut counts = Counts::new();
 
-    todo!("implement solution here");
+    for num in parser.iter_parse::<Binary>() {
+        counts.count(&num);
+    }
 
     parser.finish()?;
 
-    println!("{}", 0);
+    let (epsilon, gamma) = counts.epsilon_gamma();
+
+    eprintln!("epsilon: {}, gamma: {}", epsilon, gamma);
+    println!("{}", epsilon * gamma);
     Ok(())
 }
 
@@ -22,4 +29,72 @@ pub fn part2(input: &mut dyn Read) -> anyhow::Result<()> {
 
     println!("{}", 0);
     Ok(())
+}
+
+struct Binary {
+    digits: Vec<bool>,
+}
+
+impl FromStr for Binary {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut digits = Vec::new();
+        for ch in s.chars() {
+            match ch {
+                '0' => digits.push(false),
+                '1' => digits.push(true),
+                _ => return Err(()),
+            }
+        }
+        Ok(Binary { digits })
+    }
+}
+
+struct Counts {
+    num_digits: usize,
+    zeros: Vec<u32>,
+    ones: Vec<u32>,
+}
+
+impl Counts {
+    pub fn new() -> Self {
+        Self {
+            num_digits: 0,
+            ones: Vec::new(),
+            zeros: Vec::new(),
+        }
+    }
+
+    pub fn count(&mut self, num: &Binary) {
+        let num_digits = num.digits.len();
+        self.num_digits = num_digits.max(self.num_digits);
+        self.zeros.resize(self.num_digits, 0);
+        self.ones.resize(self.num_digits, 0);
+
+        for (i, d) in num.digits.iter().enumerate() {
+            if *d {
+                self.ones[i] += 1;
+            } else {
+                self.zeros[i] += 1;
+            }
+        }
+    }
+
+    pub fn epsilon_gamma(&self) -> (u64, u64) {
+        let mut epsilon = 0;
+        let mut gamma = 0;
+        for (i, (c0, c1)) in self.zeros.iter().zip(self.ones.iter()).enumerate() {
+            epsilon *= 2;
+            gamma *= 2;
+            if c0 > c1 {
+                epsilon += 1;
+            } else if c0 < c1 {
+                gamma += 1;
+            } else {
+                eprintln!("tie at bit {}", i);
+            }
+        }
+        (epsilon, gamma)
+    }
 }
--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -1,8 +1,18 @@
-use advent_of_code_2021::{aoc_main, Day, day1, day2};
+use advent_of_code_2021::{aoc_main, day1, day2, day3, Day};
 
 fn main() -> anyhow::Result<()> {
     aoc_main(&[
-        Day { first: day1::part1, second: day1::part2 },
-        Day { first: day2::part1, second: day2::part2 },
+        Day {
+            first: day1::part1,
+            second: day1::part2,
+        },
+        Day {
+            first: day2::part1,
+            second: day2::part2,
+        },
+        Day {
+            first: day3::part1,
+            second: day3::part2,
+        },
     ])
 }
