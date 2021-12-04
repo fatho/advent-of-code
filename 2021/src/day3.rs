ec2e90955d1dea384e20e37f126e34cd88109665
Fabian Thorand <f.thorand@gmail.com>
2021-12-04 17:29:32 +0100

    Improve performance of day3

--- 2021/src/day3.rs
+++ 2021/src/day3.rs
@@ -1,7 +1,7 @@
 use nom::branch::alt;
 use nom::bytes::complete::tag;
-use nom::combinator::{map, value};
-use nom::multi::{fold_many0, many0};
+use nom::combinator::{flat_map, map, value};
+use nom::multi::fold_many0;
 use nom::sequence::terminated;
 use nom::IResult;
 
@@ -12,136 +12,107 @@ pub static RUN: Day = Day { part1, part2 };
 
 pub fn part1(input: &[u8]) -> anyhow::Result<i64> {
     parsers::parse(
-        map(
-            fold_many0(
-                terminated(Binary::parse, parsers::newline),
-                || Counts::new(),
-                |mut counts, num| {
-                    counts.count(&num);
-                    counts
+        flat_map(terminated(BinaryLen::parse, parsers::newline), |first| {
+            map(
+                fold_many0(
+                    terminated(parse_bin, parsers::newline),
+                    move || {
+                        let mut counts = Counts::new(first.len as usize);
+                        counts.count(first.value);
+                        counts
+                    },
+                    |mut counts, num| {
+                        counts.count(num);
+                        counts
+                    },
+                ),
+                |counts| {
+                    let (epsilon, gamma) = counts.epsilon_gamma();
+                    log::debug!("epsilon: {}, gamma: {}", epsilon, gamma);
+                    (epsilon * gamma) as i64
                 },
-            ),
-            |counts| {
-                let (epsilon, gamma) = counts.epsilon_gamma();
-                log::debug!("epsilon: {}, gamma: {}", epsilon, gamma);
-                (epsilon * gamma) as i64
-            },
-        ),
+            )
+        }),
         input,
     )
 }
 
 pub fn part2(input: &[u8]) -> anyhow::Result<i64> {
     parsers::parse(
-        map(many0(terminated(Binary::parse, parsers::newline)), |nums| {
-            // count bits of all numbers
-            let mut counts = Counts::new();
-            for num in nums.iter() {
-                counts.count(num);
-            }
-            // determine o2 number
-            let mut o2_candidates = nums.clone();
-            {
-                let mut o2_counts = counts.clone();
-                let mut bit = 0;
-                while o2_candidates.len() > 1 {
-                    let mcb = o2_counts.most_common_bit(bit).unwrap_or(true);
-                    o2_candidates.retain(|num| {
-                        let keep = num.bit(bit) == mcb;
-                        if ! keep {
-                            o2_counts.uncount(num);
+        flat_map(terminated(BinaryLen::parse, parsers::newline), |first| {
+            map(
+                fold_many0(
+                    terminated(parse_bin, parsers::newline),
+                    move || {
+                        let nums = vec![first.value];
+                        let mut counts = Counts::new(first.len as usize);
+                        counts.count(first.value);
+                        (counts, nums)
+                    },
+                    |(mut counts, mut nums), num| {
+                        counts.count(num);
+                        nums.push(num);
+                        (counts, nums)
+                    },
+                ),
+                |(counts, nums)| {
+                    // determine o2 number
+                    let mut o2_candidates = nums.clone();
+                    {
+                        let mut o2_counts = counts.clone();
+                        let mut bit = counts.num_digits;
+                        while o2_candidates.len() > 1 {
+                            bit -= 1;
+                            let mcb = o2_counts.most_common_bit(bit).unwrap_or(true);
+                            o2_candidates.retain(|num| {
+                                let keep = (num & (1 << bit) != 0) == mcb;
+                                if !keep {
+                                    o2_counts.uncount(*num);
+                                }
+                                keep
+                            });
                         }
-                        keep
-                    });
-                    bit += 1;
-                }
-            }
+                    }
 
-            let mut co2_candidates = nums;
-            {
-                let mut co2_counts = counts.clone();
-                let mut bit = 0;
-                while co2_candidates.len() > 1 {
-                    let lcb = ! co2_counts.most_common_bit(bit).unwrap_or(true);
-                    co2_candidates.retain(|num| {
-                        let keep = num.bit(bit) == lcb;
-                        if ! keep {
-                            co2_counts.uncount(num);
+                    let mut co2_candidates = nums;
+                    {
+                        let mut co2_counts = counts.clone();
+                        let mut bit = counts.num_digits;
+                        while co2_candidates.len() > 1 {
+                            bit -= 1;
+                            let lcb = !co2_counts.most_common_bit(bit).unwrap_or(true);
+                            co2_candidates.retain(|num| {
+                                let keep = (num & (1 << bit) != 0) == lcb;
+                                if !keep {
+                                    co2_counts.uncount(*num);
+                                }
+                                keep
+                            });
                         }
-                        keep
-                    });
-                    bit += 1;
-                }
-            }
+                    }
 
-            log::debug!("{:?}", o2_candidates[0]);
-            let o2 = o2_candidates[0].to_u64();
-            let co2 = co2_candidates[0].to_u64();
+                    log::debug!("{:?}", o2_candidates[0]);
+                    let o2 = o2_candidates[0];
+                    let co2 = co2_candidates[0];
 
-            log::debug!("o2: {}, co2: {}", o2, co2);
-            (o2 * co2) as i64
+                    log::debug!("o2: {}, co2: {}", o2, co2);
+                    (o2 * co2) as i64
+                },
+            )
         }),
         input,
     )
 }
 
-#[derive(Debug, Copy, Clone)]
-struct Binary {
-    value: u32,
-    len: u32,
+pub fn parse_bin(input: &[u8]) -> IResult<&[u8], u32> {
+    fold_many0(
+        alt((value(0, tag(b"0")), value(1, tag(b"1")))),
+        || 0,
+        |value, digit| (value << 1) | digit,
+    )(input)
 }
 
-impl Binary {
-    pub fn parse(input: &[u8]) -> IResult<&[u8], Binary> {
-        map(
-            fold_many0(
-                alt((value(0, tag(b"0")), value(1, tag(b"1")))),
-                || (0, 0),
-                |(len, value), digit| {
-                    (
-                        len + 1,
-                        (value << 1) | digit,
-                    )
-                },
-            ),
-            |(len, value)| Binary { len, value },
-        )(input)
-    }
-
-    pub fn bit(self, index: usize) -> bool {
-        self.value & (1 << (self.len - 1 - index as u32)) != 0
-    }
-
-    pub fn to_u64(&self) -> u64 {
-        self.value as u64
-    }
-
-    pub fn digits(&self) -> DigitIter {
-        DigitIter {
-            index: self.len,
-            value: self.value,
-        }
-    }
-}
-struct DigitIter {
-    index: u32,
-    value: u32,
-}
-
-impl Iterator for DigitIter {
-    type Item = bool;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.index > 0 {
-            self.index -= 1;
-            Some(self.value & (1 << self.index) != 0)
-        } else {
-            None
-        }
-    }
-}
-
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Default)]
 struct Counts {
     num_digits: usize,
     zeros: Vec<u32>,
@@ -149,10 +120,10 @@ struct Counts {
 }
 
 impl Counts {
-    pub fn new() -> Self {
+    pub fn new(num_digits: usize) -> Self {
         Self {
-            num_digits: 0,
-            zeros: Vec::new(),
+            num_digits,
+            zeros: vec![0; num_digits],
             total: 0,
         }
     }
@@ -165,45 +136,43 @@ impl Counts {
         self.total - self.zeros[index]
     }
 
-    pub fn count(&mut self, num: &Binary) {
-        let num_digits = num.len as usize;
-        self.num_digits = num_digits.max(self.num_digits);
+    pub fn count(&mut self, mut num: u32) {
         self.total += 1;
-        self.zeros.resize(self.num_digits, 0);
-
-        for (i, d) in num.digits().enumerate() {
-            if !d {
+        for i in 0..self.num_digits {
+            if num & 1 == 0 {
                 self.zeros[i] += 1;
             }
+            num >>= 1;
         }
     }
 
-    pub fn uncount(&mut self, num: &Binary) {
+    pub fn uncount(&mut self, mut num: u32) {
         self.total -= 1;
 
-        for (i, d) in num.digits().enumerate() {
-            if !d {
+        for i in 0..self.num_digits {
+            if num & 1 == 0 {
                 self.zeros[i] -= 1;
             }
+            num >>= 1;
         }
     }
 
     pub fn epsilon_gamma(&self) -> (u64, u64) {
         let mut epsilon = 0;
         let mut gamma = 0;
+        let mut mask = 1;
         for i in 0..self.num_digits {
-            epsilon *= 2;
-            gamma *= 2;
             match self.most_common_bit(i) {
                 Some(bit) => {
                     if bit {
-                        gamma += 1;
+                        gamma |= mask;
                     } else {
-                        epsilon += 1;
+                        epsilon |= mask;
                     }
                 }
                 None => log::error!("tie at bit {}", i),
             }
+            mask <<= 1;
         }
         (epsilon, gamma)
     }
@@ -214,6 +183,25 @@ impl Counts {
             Ordering::Equal => None,
             Ordering::Greater => Some(false),
         }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+struct BinaryLen {
+    value: u32,
+    len: u32,
+}
+
+impl BinaryLen {
+    pub fn parse(input: &[u8]) -> IResult<&[u8], BinaryLen> {
+        map(
+            fold_many0(
+                alt((value(0, tag(b"0")), value(1, tag(b"1")))),
+                || (0, 0),
+                |(len, value), digit| (len + 1, (value << 1) | digit),
+            ),
+            |(len, value)| BinaryLen { len, value },
+        )(input)
     }
 }
 
