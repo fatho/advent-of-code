cc3e1908ee7763eec9f6b6c97a6da399950c5994
Fabian Thorand <f.thorand@gmail.com>
2021-12-04 17:08:35 +0100

    Improve day3 performance

--- 2021/src/day3.rs
+++ 2021/src/day3.rs
@@ -1,8 +1,9 @@
-use nom::character::complete::one_of;
-use nom::combinator::map;
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+use nom::combinator::{map, value};
 use nom::multi::{fold_many0, many0};
-use nom::IResult;
 use nom::sequence::terminated;
+use nom::IResult;
 
 use crate::{parsers, Day};
 use std::cmp::Ordering;
@@ -18,59 +19,69 @@ pub fn part1(input: &[u8]) -> anyhow::Result<i64> {
                 |mut counts, num| {
                     counts.count(&num);
                     counts
-                }
+                },
             ),
             |counts| {
                 let (epsilon, gamma) = counts.epsilon_gamma();
                 log::debug!("epsilon: {}, gamma: {}", epsilon, gamma);
                 (epsilon * gamma) as i64
-            }
+            },
         ),
-        input
+        input,
     )
 }
 
 pub fn part2(input: &[u8]) -> anyhow::Result<i64> {
     parsers::parse(
-        map(
-            many0(
-                terminated(Binary::parse, parsers::newline),
-            ),
-            |nums| {
-
-                let mut o2_candidates = nums.clone();
+        map(many0(terminated(Binary::parse, parsers::newline)), |nums| {
+            // count bits of all numbers
+            let mut counts = Counts::new();
+            for num in nums.iter() {
+                counts.count(num);
+            }
+            // determine o2 number
+            let mut o2_candidates = nums.clone();
+            {
+                let mut o2_counts = counts.clone();
                 let mut bit = 0;
                 while o2_candidates.len() > 1 {
-                    let mut counts = Counts::new();
-                    for num in o2_candidates.iter() {
-                        counts.count(num);
-                    }
-                    let mcb = counts.most_common_bit(bit).unwrap_or(true);
-                    o2_candidates.retain(|num| num.bit(bit) == mcb);
+                    let mcb = o2_counts.most_common_bit(bit).unwrap_or(true);
+                    o2_candidates.retain(|num| {
+                        let keep = num.bit(bit) == mcb;
+                        if ! keep {
+                            o2_counts.uncount(num);
+                        }
+                        keep
+                    });
                     bit += 1;
                 }
+            }
 
-                let mut co2_candidates = nums;
+            let mut co2_candidates = nums;
+            {
+                let mut co2_counts = counts.clone();
                 let mut bit = 0;
                 while co2_candidates.len() > 1 {
-                    let mut counts = Counts::new();
-                    for num in co2_candidates.iter() {
-                        counts.count(num);
-                    }
-                    let lcb = !counts.most_common_bit(bit).unwrap_or(true);
-                    co2_candidates.retain(|num| num.bit(bit) == lcb);
+                    let lcb = ! co2_counts.most_common_bit(bit).unwrap_or(true);
+                    co2_candidates.retain(|num| {
+                        let keep = num.bit(bit) == lcb;
+                        if ! keep {
+                            co2_counts.uncount(num);
+                        }
+                        keep
+                    });
                     bit += 1;
                 }
-
-                log::debug!("{:?}", o2_candidates[0]);
-                let o2 = o2_candidates[0].to_u64();
-                let co2 = co2_candidates[0].to_u64();
-
-                log::debug!("o2: {}, co2: {}", o2, co2);
-                (o2 * co2) as i64
             }
-        ),
-        input
+
+            log::debug!("{:?}", o2_candidates[0]);
+            let o2 = o2_candidates[0].to_u64();
+            let co2 = co2_candidates[0].to_u64();
+
+            log::debug!("o2: {}, co2: {}", o2, co2);
+            (o2 * co2) as i64
+        }),
+        input,
     )
 }
 
@@ -84,16 +95,12 @@ impl Binary {
     pub fn parse(input: &[u8]) -> IResult<&[u8], Binary> {
         map(
             fold_many0(
-                one_of(b"01".as_ref()),
+                alt((value(0, tag(b"0")), value(1, tag(b"1")))),
                 || (0, 0),
                 |(len, value), digit| {
                     (
                         len + 1,
-                        if digit == '1' {
-                            (value << 1) | 1
-                        } else {
-                            value << 1
-                        },
+                        (value << 1) | digit,
                     )
                 },
             ),
@@ -134,6 +141,7 @@ impl Iterator for DigitIter {
     }
 }
 
+#[derive(Debug, Clone)]
 struct Counts {
     num_digits: usize,
     zeros: Vec<u32>,
@@ -166,6 +174,16 @@ impl Counts {
         for (i, d) in num.digits().enumerate() {
             if !d {
                 self.zeros[i] += 1;
+            }
+        }
+    }
+
+    pub fn uncount(&mut self, num: &Binary) {
+        self.total -= 1;
+
+        for (i, d) in num.digits().enumerate() {
+            if !d {
+                self.zeros[i] -= 1;
             }
         }
     }
