d4e6513ca23cb492bd5efd31e48a4c938fbe9bfc
Fabian Thorand <f.thorand@gmail.com>
2021-12-04 21:06:14 +0100

    Prepare day5

---
+++ 2021/src/day5.rs
@@ -0,0 +1,27 @@
+use nom::bytes::complete::{take_while};
+use nom::combinator::{flat_map, map};
+use nom::multi::{fold_many0};
+use nom::sequence::terminated;
+use nom::IResult;
+
+use crate::{parsers, Day};
+use std::cmp::Ordering;
+
+pub static RUN: Day = Day { part1, part2 };
+
+pub fn part1<'a>(input: &'a [u8]) -> anyhow::Result<i64> {
+    parsers::parse(
+        |_| Ok((b"", 0)),
+        input,
+    )
+}
+
+pub fn part2(input: &[u8]) -> anyhow::Result<i64> {
+    parsers::parse(
+        |_| Ok((b"", 0)),
+        input,
+    )
+}
+
+
+crate::test_day!(crate::day5::RUN, "day5", 0, 0);
--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -1,5 +1,5 @@
-use advent_of_code_2021::{aoc_main, day1, day2, day3, day4};
+use advent_of_code_2021::{aoc_main, day1, day2, day3, day4, day5};
 
 fn main() -> anyhow::Result<()> {
-    aoc_main(&[day1::RUN, day2::RUN, day3::RUN, day4::RUN])
+    aoc_main(&[day1::RUN, day2::RUN, day3::RUN, day4::RUN, day5::RUN])
 }
