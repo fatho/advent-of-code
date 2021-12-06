e478e9a32e2ef4304f1ef818f343b1470e99f5aa
Fabian Thorand <f.thorand@gmail.com>
2021-12-06 09:38:20 +0100

    Prepare remaining days

---
+++ 2021/src/day11.rs
@@ -0,0 +1,19 @@
+#![allow(unused_imports)]
+
+use crate::{parsers, Day};
+use nom::bytes::complete::take_while;
+use nom::combinator::{flat_map, map};
+use nom::multi::fold_many0;
+use nom::sequence::terminated;
+use nom::IResult;
+pub static RUN: Day = Day { part1, part2 };
+
+pub fn part1(input: &[u8]) -> anyhow::Result<i64> {
+    parsers::parse(|_| Ok((b"", 0)), input)
+}
+
+pub fn part2(input: &[u8]) -> anyhow::Result<i64> {
+    parsers::parse(|_| Ok((b"", 0)), input)
+}
+
+crate::test_day!(crate::day11::RUN, "day11", 0, 0);
--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -1,5 +1,31 @@
-use advent_of_code_2021::{aoc_main, day1, day2, day3, day4, day5, day6};
+use advent_of_code_2021::{self as aoc, aoc_main};
 
 fn main() -> anyhow::Result<()> {
-    aoc_main(&[day1::RUN, day2::RUN, day3::RUN, day4::RUN, day5::RUN, day6::RUN])
+    aoc_main(&[
+        aoc::day1::RUN,
+        aoc::day2::RUN,
+        aoc::day3::RUN,
+        aoc::day4::RUN,
+        aoc::day5::RUN,
+        aoc::day6::RUN,
+        aoc::day7::RUN,
+        aoc::day8::RUN,
+        aoc::day9::RUN,
+        aoc::day10::RUN,
+        aoc::day11::RUN,
+        aoc::day12::RUN,
+        aoc::day13::RUN,
+        aoc::day14::RUN,
+        aoc::day15::RUN,
+        aoc::day16::RUN,
+        aoc::day17::RUN,
+        aoc::day18::RUN,
+        aoc::day19::RUN,
+        aoc::day20::RUN,
+        aoc::day21::RUN,
+        aoc::day22::RUN,
+        aoc::day23::RUN,
+        aoc::day24::RUN,
+        aoc::day25::RUN,
+    ])
 }
