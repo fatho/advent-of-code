d51e442cbf56ecbc72e0d485fcfc1ea0b7452fee
Fabian Thorand <f.thorand@gmail.com>
2021-12-08 20:22:55 +0100

    day7: use linear time algorithm for part 1

--- 2021/src/day7.rs
+++ 2021/src/day7.rs
@@ -11,7 +11,38 @@ pub static RUN: Day = Day { part1, part2 };
 pub fn part1(input: &[u8]) -> anyhow::Result<i64> {
     let crab_counts = parse_crabs(input)?;
 
-    let best_fuel = compute_target(&crab_counts, abs_diff);
+    // At position 0, there are 0 crabs to the left of us, and hence no fuel
+    // needs to be spent by crabs on the left to get to position 0.
+    let mut left_fuel = 0;
+    let mut left_crabs = 0;
+    // At position 0, all crabs to the right of us need to pay fuel.
+    // Count the crabs and the fuel requirements together:
+    let (mut right_fuel, mut right_crabs) = crab_counts[1..].iter().enumerate().fold(
+        (0, 0),
+        |(right_fuel, right_crabs), (pos, crabs)| {
+            (right_fuel + (pos + 1) as u32 * crabs, right_crabs + crabs)
+        },
+    );
+
+    let mut best_fuel = right_fuel;
+    for (prev, cur) in crab_counts.iter().zip(crab_counts[1..].iter()) {
+        // Since we moved one position over, all crabs to the right need to use
+        // one unit of fuel less.
+        right_fuel -= right_crabs;
+        // And we now have fewer crabs on the right, as the first position on
+        // the right became the current position.
+        right_crabs -= cur;
+        // The crabs from our previous position are now to the left.
+        left_crabs += prev;
+        // And since we moved the target, all crabs to the left need to pay one
+        // fuel extra.
+        left_fuel += left_crabs;
+
+        let fuel = left_fuel + right_fuel;
+        if fuel < best_fuel {
+            best_fuel = fuel;
+        }
+    }
 
     Ok(best_fuel as i64)
 }
