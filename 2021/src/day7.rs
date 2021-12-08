7cb17820da4d7f22f743a205a0ff3af43dbf4ed2
Fabian Thorand <f.thorand@gmail.com>
2021-12-08 21:09:13 +0100

    day7: one algorithm to rule them all
    And it's fast, too.

--- 2021/src/day7.rs
+++ 2021/src/day7.rs
@@ -11,38 +11,7 @@ pub static RUN: Day = Day { part1, part2 };
 pub fn part1(input: &[u8]) -> anyhow::Result<i64> {
     let crab_counts = parse_crabs(input)?;
 
-    // At position 0, there are 0 crabs to the left of us, and hence no fuel
-    // needs to be spent by crabs on the left to get to position 0.
-    let mut left_fuel = 0;
-    let mut left_crabs = 0;
-    // At position 0, all crabs to the right of us need to pay fuel.
-    // Count the crabs and the fuel requirements together:
-    let (mut right_fuel, mut right_crabs) = crab_counts[1..].iter().enumerate().fold(
-        (0, 0),
-        |(right_fuel, right_crabs), (pos, crabs)| {
-            (right_fuel + (pos + 1) as u32 * crabs, right_crabs + crabs)
-        },
-    );
-
-    let mut best_fuel = right_fuel;
-    for (prev, cur) in crab_counts.iter().zip(crab_counts[1..].iter()) {
-        // Since we moved one position over, all crabs to the right need to use
-        // one unit of fuel less.
-        right_fuel -= right_crabs;
-        // And we now have fewer crabs on the right, as the first position on
-        // the right became the current position.
-        right_crabs -= cur;
-        // The crabs from our previous position are now to the left.
-        left_crabs += prev;
-        // And since we moved the target, all crabs to the left need to pay one
-        // fuel extra.
-        left_fuel += left_crabs;
-
-        let fuel = left_fuel + right_fuel;
-        if fuel < best_fuel {
-            best_fuel = fuel;
-        }
-    }
+    let best_fuel = compute_fuel::<2>(&crab_counts);
 
     Ok(best_fuel as i64)
 }
@@ -50,36 +19,46 @@ pub fn part1(input: &[u8]) -> anyhow::Result<i64> {
 pub fn part2(input: &[u8]) -> anyhow::Result<i64> {
     let crab_counts = parse_crabs(input)?;
 
-    let best_fuel = compute_target(&crab_counts, tri_sum_diff);
+    let best_fuel = compute_fuel::<3>(&crab_counts);
 
     Ok(best_fuel as i64)
 }
 
-fn tri_sum_diff(target: usize, pos: usize) -> usize {
-    let delta = abs_diff(target, pos);
-    (delta * (delta + 1)) / 2
-}
+fn compute_fuel<const ORDER: usize>(crab_counts: &[u32]) -> u64 {
+    // Running total of fuel aggregation on the left
+    let mut left = [0_u64; ORDER];
 
-fn abs_diff(target: usize, pos: usize) -> usize {
-    if pos > target {
-        pos - target
-    } else {
-        target - pos
-    }
-}
+    // Running total of fuel aggregation on the right
+    let mut right = crab_counts[1..]
+        .iter()
+        .rev()
+        .fold([0_u64; ORDER], |mut right, crabs| {
+            right[0] += *crabs as u64;
+            for order_index in 1..ORDER {
+                right[order_index] += right[order_index - 1];
+            }
+            right
+        });
 
-fn compute_target<F: Fn(usize, usize) -> usize>(crab_counts: &[u32], distance: F) -> u64 {
-    // brute force
-    let mut best_fuel = u64::MAX;
-    for target in 0..crab_counts.len() {
-        let mut fuel = 0_u64;
-        for (pos, count) in crab_counts.iter().enumerate() {
-            fuel += *count as u64 * distance(pos, target) as u64;
+    let mut best_fuel = right[ORDER - 1];
+    for (prev, cur) in crab_counts.iter().zip(crab_counts[1..].iter()) {
+        // Take fuel from the right away
+        for order_index in (1..ORDER).rev() {
+            right[order_index] -= right[order_index - 1];
         }
+        right[0] -= *cur as u64;
+        // And put it to the left
+        left[0] += *prev as u64;
+        for order_index in 1..ORDER {
+            left[order_index] += left[order_index - 1];
+        }
+
+        let fuel = left[ORDER - 1] + right[ORDER - 1];
         if fuel < best_fuel {
             best_fuel = fuel;
         }
     }
+
     best_fuel
 }
 
