3d38cdd820ac42b793317ab75bd4235babbe0651
Fabian Thorand <f.thorand@gmail.com>
2021-12-19 12:30:14 +0100

    day19: implement vector operations

--- 2021/src/day19.rs
+++ 2021/src/day19.rs
@@ -1,5 +1,7 @@
 #![allow(unused_imports)]
 
+use std::ops::{Add, Mul};
+
 use crate::{parsers, Day};
 use nom::bytes::complete::{tag, take_while};
 use nom::character::complete as numbers;
@@ -39,31 +41,135 @@ fn p_scanner(input: &[u8]) -> IResult<&[u8], Scanner> {
     )(input)
 }
 
-fn p_relpnt(input: &[u8]) -> IResult<&[u8], RelPnt> {
+fn p_relpnt(input: &[u8]) -> IResult<&[u8], Vec3> {
     map(
         tuple((numbers::i32, tag(","), numbers::i32, tag(","), numbers::i32)),
-        |(x, _, y, _, z)| RelPnt { x, y, z },
+        |(x, _, y, _, z)| Vec3 { x, y, z },
     )(input)
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
-struct RelPnt {
+struct Vec3 {
     x: i32,
     y: i32,
     z: i32,
 }
 
+impl Vec3 {
+    pub const fn new(x: i32, y: i32, z: i32) -> Self {
+        Self { x, y, z }
+    }
+
+    pub const fn cross(self, other: Vec3) -> Self {
+        Vec3 {
+            x: self.y * other.z - self.z * other.y,
+            y: self.z * other.x - self.x * other.z,
+            z: self.x * other.y - self.y * other.x,
+        }
+    }
+
+    pub const fn dot(self, other: Vec3) -> i32 {
+        self.x * other.x + self.y * other.y + self.z * other.z
+    }
+}
+
+impl Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec3 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl Mul<i32> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: i32) -> Self::Output {
+        Vec3 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct Scanner {
     id: u32,
-    points: Vec<RelPnt>,
+    points: Vec<Vec3>,
 }
+
+#[derive(Clone, Copy, PartialEq, Eq)]
+struct Orientation {
+    fwd: Vec3,
+    up: Vec3,
+    right: Vec3, // TODO: or is it left?
+}
+
+impl Orientation {
+    pub const fn new(fwd: Vec3, up: Vec3) -> Self {
+        Self {
+            fwd,
+            up,
+            right: fwd.cross(up),
+        }
+    }
+
+    pub fn local_to_global(&self, v: Vec3) -> Vec3 {
+        self.fwd * v.x + self.up * v.y + self.right * v.z
+    }
+
+    pub fn global_to_local(&self, v: Vec3) -> Vec3 {
+        Vec3 {
+            x: self.fwd.dot(v),
+            y: self.up.dot(v),
+            z: self.right.dot(v),
+        }
+    }
+}
+
+const ORIENTATIONS: [Orientation; 24] = [
+    // +X
+    Orientation::new(Vec3::new(1, 0, 0), Vec3::new(0, 1, 0)),
+    Orientation::new(Vec3::new(1, 0, 0), Vec3::new(0, 0, 1)),
+    Orientation::new(Vec3::new(1, 0, 0), Vec3::new(0, -1, 0)),
+    Orientation::new(Vec3::new(1, 0, 0), Vec3::new(0, 0, -1)),
+    // -X
+    Orientation::new(Vec3::new(-1, 0, 0), Vec3::new(0, 1, 0)),
+    Orientation::new(Vec3::new(-1, 0, 0), Vec3::new(0, 0, 1)),
+    Orientation::new(Vec3::new(-1, 0, 0), Vec3::new(0, -1, 0)),
+    Orientation::new(Vec3::new(-1, 0, 0), Vec3::new(0, 0, -1)),
+    // +Y
+    Orientation::new(Vec3::new(0, 1, 0), Vec3::new(1, 0, 0)),
+    Orientation::new(Vec3::new(0, 1, 0), Vec3::new(0, 0, 1)),
+    Orientation::new(Vec3::new(0, 1, 0), Vec3::new(-1, 0, 0)),
+    Orientation::new(Vec3::new(0, 1, 0), Vec3::new(0, 0, -1)),
+    // -Y
+    Orientation::new(Vec3::new(0, -1, 0), Vec3::new(1, 0, 0)),
+    Orientation::new(Vec3::new(0, -1, 0), Vec3::new(0, 0, 1)),
+    Orientation::new(Vec3::new(0, -1, 0), Vec3::new(-1, 0, 0)),
+    Orientation::new(Vec3::new(0, -1, 0), Vec3::new(0, 0, -1)),
+    // +Z
+    Orientation::new(Vec3::new(0, 0, 1), Vec3::new(1, 0, 0)),
+    Orientation::new(Vec3::new(0, 0, 1), Vec3::new(0, 1, 0)),
+    Orientation::new(Vec3::new(0, 0, 1), Vec3::new(-1, 0, 0)),
+    Orientation::new(Vec3::new(0, 0, 1), Vec3::new(0, -1, 0)),
+    // -Z
+    Orientation::new(Vec3::new(0, 0, -1), Vec3::new(1, 0, 0)),
+    Orientation::new(Vec3::new(0, 0, -1), Vec3::new(0, 1, 0)),
+    Orientation::new(Vec3::new(0, 0, -1), Vec3::new(-1, 0, 0)),
+    Orientation::new(Vec3::new(0, 0, -1), Vec3::new(0, -1, 0)),
+];
 
 #[test]
 fn test_p_relpnt() {
     assert_eq!(
         p_relpnt(b"1,-2,3").unwrap(),
-        (b"".as_ref(), RelPnt { x: 1, y: -2, z: 3 })
+        (b"".as_ref(), Vec3 { x: 1, y: -2, z: 3 })
     );
 }
 
@@ -82,12 +188,12 @@ fn test_p_scanner() {
             Scanner {
                 id: 0,
                 points: vec![
-                    RelPnt {
+                    Vec3 {
                         x: 602,
                         y: 365,
                         z: -604
                     },
-                    RelPnt {
+                    Vec3 {
                         x: 309,
                         y: -819,
                         z: -775
@@ -101,12 +207,12 @@ fn test_p_scanner() {
 #[test]
 fn test_p_input() {
     let points = vec![
-        RelPnt {
+        Vec3 {
             x: 602,
             y: 365,
             z: -604,
         },
-        RelPnt {
+        Vec3 {
             x: 309,
             y: -819,
             z: -775,
