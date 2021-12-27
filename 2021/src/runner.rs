ea4cdf40c95515f528f2529b5afce94962f968fe
Fabian Thorand <f.thorand@gmail.com>
2021-12-27 18:32:10 +0100

    Add profiling goody

--- 2021/src/runner.rs
+++ 2021/src/runner.rs
@@ -51,6 +51,10 @@ pub struct AocOpt {
     #[structopt(short, long, conflicts_with_all(&["day", "part"]))]
     all: bool,
 
+    /// Repeat the computation many times for easier flamegraphing.
+    #[structopt(short, long, default_value("1"))]
+    repeat: usize,
+
     /// Input file
     #[structopt(short, long, parse(from_os_str))]
     input: PathBuf,
@@ -76,45 +80,49 @@ impl Day {
 
 pub fn aoc_main(days: &[Day]) -> anyhow::Result<()> {
     let opt = AocOpt::from_args();
-    if opt.all {
-        let before = Instant::now();
-        for (index, day) in days.iter().enumerate() {
-            let inpath = opt.input.join(format!("day{}/input.txt", index + 1));
-            let contents =
-                read_bytes(&inpath).with_context(|| format!("reading {}", inpath.display()))?;
-
+    for _ in 0..opt.repeat {
+        if opt.all {
             let before = Instant::now();
-            let out1 = (day.part1)(&contents)
-                .with_context(|| format!("day{}.1: {}", index + 1, inpath.display().to_string()))?;
-            let after_part1 = Instant::now();
-            let out2 = (day.part2)(&contents)
-                .with_context(|| format!("day{}.2: {}", index + 1, inpath.display().to_string()))?;
-            let after_part2 = Instant::now();
+            for (index, day) in days.iter().enumerate() {
+                let inpath = opt.input.join(format!("day{}/input.txt", index + 1));
+                let contents =
+                    read_bytes(&inpath).with_context(|| format!("reading {}", inpath.display()))?;
 
-            println!(
-                "{}\t{:?}\t{:?}\t{:.3}\t{:.3}",
-                index + 1,
-                out1,
-                out2,
-                after_part1.duration_since(before).as_secs_f64() * 1000.0,
-                after_part2.duration_since(after_part1).as_secs_f64() * 1000.0,
-            );
+                let before = Instant::now();
+                let out1 = (day.part1)(&contents).with_context(|| {
+                    format!("day{}.1: {}", index + 1, inpath.display().to_string())
+                })?;
+                let after_part1 = Instant::now();
+                let out2 = (day.part2)(&contents).with_context(|| {
+                    format!("day{}.2: {}", index + 1, inpath.display().to_string())
+                })?;
+                let after_part2 = Instant::now();
+
+                println!(
+                    "{}\t{:?}\t{:?}\t{:.3}\t{:.3}",
+                    index + 1,
+                    out1,
+                    out2,
+                    after_part1.duration_since(before).as_secs_f64() * 1000.0,
+                    after_part2.duration_since(after_part1).as_secs_f64() * 1000.0,
+                );
+            }
+            let duration = before.elapsed();
+            eprintln!("Took {:.3} ms", duration.as_secs_f64() * 1000.0);
+        } else if let Some(day) = days.get((opt.day.unwrap() - 1) as usize) {
+            let runner = match opt.part {
+                Part::One => day.part1,
+                Part::Two => day.part2,
+            };
+            let before = Instant::now();
+            let contents = read_bytes(&opt.input)?;
+            let output = runner(&contents)?;
+            println!("{}", output);
+            let duration = before.elapsed();
+            eprintln!("Took {:.3} ms", duration.as_secs_f64() * 1000.0);
+        } else {
+            anyhow::bail!("no such day")
         }
-        let duration = before.elapsed();
-        eprintln!("Took {:.3} ms", duration.as_secs_f64() * 1000.0);
-    } else if let Some(day) = days.get((opt.day.unwrap() - 1) as usize) {
-        let runner = match opt.part {
-            Part::One => day.part1,
-            Part::Two => day.part2,
-        };
-        let before = Instant::now();
-        let contents = read_bytes(&opt.input)?;
-        let output = runner(&contents)?;
-        println!("{}", output);
-        let duration = before.elapsed();
-        eprintln!("Took {:.3} ms", duration.as_secs_f64() * 1000.0);
-    } else {
-        anyhow::bail!("no such day")
     }
     Ok(())
 }
