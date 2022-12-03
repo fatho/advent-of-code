d7be58510e2ea6185bd676e72cd5cd83de00a439
Fabian Thorand <f.thorand@gmail.com>
2022-12-03 09:09:42 +0100

    Improve performance of day 3 part 2

--- 2022/src/day3.rs
+++ 2022/src/day3.rs
@@ -2,19 +2,9 @@
 
 use std::collections::HashSet;
 
-use anyhow::bail;
-use nom::{
-    character::complete::alpha1,
-    combinator::flat_map,
-    multi::{fold_many0, fold_many_m_n},
-    sequence::terminated,
-    IResult,
-};
+use anyhow::{anyhow, bail};
 
-use crate::{
-    parsers::{self, newline},
-    Day,
-};
+use crate::Day;
 
 pub static RUN: Day = Day { part1, part2 };
 
@@ -25,7 +15,7 @@ pub fn part1(input: &[u8]) -> anyhow::Result<String> {
         if line.len() & 1 == 1 {
             bail!("Rucksack with odd number of items on line {}", index + 1);
         } else if line.is_empty() {
-            continue;
+            break;
         }
 
         let mid = line.len() / 2;
@@ -45,96 +35,36 @@ pub fn part1(input: &[u8]) -> anyhow::Result<String> {
 }
 
 pub fn part2(input: &[u8]) -> anyhow::Result<String> {
-    let prios = parsers::parse(
-        fold_many0(
-            flat_map(
-                // Parse first rucksack in group
-                terminated(alpha1, newline),
-                |rucksack| {
-                    let item_types: HashSet<u8> = rucksack.iter().copied().collect();
-                    fold_many_m_n(
-                        2,
-                        2,
-                        terminated(alpha1, newline),
-                        move || item_types.clone(),
-                        |prev, rucksack| {
-                            rucksack
-                                .iter()
-                                .copied()
-                                .filter(|item| prev.contains(item))
-                                .collect()
-                        },
-                    )
-                },
-            ),
-            || 0,
-            |sum, common_items| {
-                debug_assert_eq!(common_items.len(), 1);
-                let badge_item = common_items.into_iter().next().unwrap();
-                sum + priority(badge_item) as u32
-            },
-        ),
-        input,
-    )?;
-    Ok(prios.to_string())
-}
+    let mut buf = input.to_owned();
+    let mut sum = 0u32;
 
-fn common_items<'a, T>(sorted_fst: &'a [T], sorted_snd: &'a [T]) -> CommonItems<'a, T> {
-    CommonItems {
-        fst: sorted_fst,
-        snd: sorted_snd,
-        ifst: 0,
-        isnd: 0,
-    }
-}
-
-struct CommonItems<'a, T> {
-    fst: &'a [T],
-    snd: &'a [T],
-    ifst: usize,
-    isnd: usize,
-}
-
-impl<'a, T: Ord> Iterator for CommonItems<'a, T> {
-    type Item = &'a T;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        while self.ifst < self.fst.len() && self.isnd < self.snd.len() {
-            match self.fst[self.ifst].cmp(&self.snd[self.isnd]) {
-                std::cmp::Ordering::Less => {
-                    self.ifst += 1;
-                }
-                std::cmp::Ordering::Equal => {
-                    let the_item = &self.fst[self.ifst];
-
-                    // Skip items of this type
-                    while self.ifst < self.fst.len() && &self.fst[self.ifst] == the_item {
-                        self.ifst += 1;
-                    }
-                    while self.isnd < self.snd.len() && &self.snd[self.isnd] == the_item {
-                        self.isnd += 1;
-                    }
-
-                    return Some(the_item);
-                }
-                std::cmp::Ordering::Greater => {
-                    self.isnd += 1;
-                }
-            }
+    let mut lines = buf.split_mut(|ch| *ch == b'\n').enumerate();
+    while let Some((index, a)) = lines.next() {
+        if a.is_empty() {
+            break;
         }
-        // As soon as one half is exhausted, there can no longer be more common items
-        None
-    }
-}
 
-#[test]
-fn test_common_items() {
-    assert_eq!(
-        common_items(&[1, 2, 2, 4, 5, 6, 6, 8, 8], &[2, 3, 4, 4, 6, 6, 7, 7])
+        let (_, b) = lines
+            .next()
+            .ok_or_else(|| anyhow!("Incomplete group at line {}", index + 2))?;
+        let (_, c) = lines
+            .next()
+            .ok_or_else(|| anyhow!("Incomplete group at line {}", index + 3))?;
+
+        a.sort();
+        b.sort();
+
+        if let Some(badge) = c
+            .iter()
             .copied()
-            .collect::<Vec<u8>>(),
-        vec![2, 4, 6]
-    );
+            .find(|item| a.binary_search(item).is_ok() && b.binary_search(item).is_ok())
+        {
+            sum += priority(badge) as u32;
+        } else {
+            bail!("No badge in group starting on line {}", index + 1);
+        }
+    }
+    Ok(sum.to_string())
 }
 
 fn priority(item: u8) -> u8 {
